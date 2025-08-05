964b542f36b76014899da796118ee0fec39ae538
Nihar Patel <niharpatel52@gmail.com>
2025-08-05 08:54:34 -0400

    Create /teams and /players routes

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,3 +1,8 @@
 class PlayersController < ApplicationController
   set :default_content_type, "application/json"
+
+  get "/players" do
+    players = Player.all
+    players.to_json(include: :team)
+  end
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,3 +1,8 @@
 class TeamsController < ApplicationController
   set :default_content_type, "application/json"
+
+  get "/teams" do
+    teams = Team.all
+    teams.to_json(include: :players)
+  end
 end
