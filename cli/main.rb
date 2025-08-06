3279dd3673bae460afa75b72a59a0dda978271a0
Nihar Patel <niharpatel52@gmail.com>
2025-08-06 09:48:53 -0400

    Create menu_handler.rb and move CLI Menu options

--- cli/main.rb
+++ cli/main.rb
@@ -1,5 +1,6 @@
 require 'rest-client'
 require 'json'
+require_relative 'menu_handler'
 
 class APIClient
   def initialize(base_url = 'http://localhost:9292')
@@ -87,60 +88,22 @@ end
 class CLIInterface
   def initialize
     @api_client = APIClient.new
-  end
-
-  def display_menu
-    puts "\n=== NFL Manager CLI ==="
-    puts "1. View all teams"
-    puts "2. View all players"
-    puts "3. View players by team ID"
-    puts "4. Create a new team"
-    puts "5. Create a new player"
-    puts "6. Move Team Location"
-    puts "7. Trade a Player"
-    puts "8. Delete a team"
-    puts "9. Delete a player"
-    puts "q. Quit"
+    @menu_handler = MenuHandler.new(self)
   end
 
   def run
-    puts "Welcome to the NFL Manager CLI!"
-    puts "This is a full CRUD App to build and manage new and existing teams and players!"
-    puts "Make sure your API server is running on http://localhost:9292"
-    puts
+    @menu_handler.show_welcome_message
 
     loop do
-      display_menu
+      @menu_handler.display_menu
       puts "Enter Your Command:"
       choice = gets.chomp.downcase
 
-      case choice
-      when '1'
-        view_all_teams
-      when '2'
-        view_all_players
-      when '3'
-        view_players_by_team_id
-      when '4'
-        create_team
-      when '5'
-        create_player
-      when '6'
-        move_team_location
-      when '7'
-        trade_player
-      when '8'
-        delete_team
-      when '9'
-        delete_player
-      when 'q', 'quit', 'exit'
-        puts "Goodbye!"
-        break
-      else
-        puts "Invalid choice. Please try again."
-      end
+      result = @menu_handler.handle_choice(choice)
+      break if result == :quit
     end
   end
+
 
   def view_all_teams
     puts "\n=== All Teams ==="
