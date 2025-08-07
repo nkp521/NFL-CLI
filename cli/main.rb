882571e79f580bcdae29eb558f9347d575f3f5ab
Nihar Patel <niharpatel52@gmail.com>
2025-08-07 15:58:09 -0400

    Create TeamOperations class and move all team activities from CLIInterface

--- cli/main.rb
+++ cli/main.rb
@@ -1,13 +1,17 @@
 require_relative 'api_client/api_client'
 require_relative 'interface/menu_handler'
 require_relative 'interface/display_helper'
+require_relative 'operations/team_operations'
 
 
 class CLIInterface
   def initialize
     @api_client = APIClient.new
-    @menu_handler = MenuHandler.new(self)
     @display = DisplayHelper.new(@api_client)
+    
+    @team_ops = TeamOperations.new(@api_client, @display)
+    
+    @menu_handler = MenuHandler.new(self)
   end
 
   def run
@@ -20,21 +24,7 @@ class CLIInterface
   end
 
   def view_all_teams
-    puts "\n=== All Teams ==="
-    response = @api_client.show_teams
-
-    if response.is_a?(Array)
-      if response.empty?
-        puts "No teams found."
-      else
-        response.each do |team|
-          @display.display_team(team)
-          puts "-" * 75
-        end
-      end
-    else
-      puts "Error: #{response[:error]}"
-    end
+    @team_ops.view_all
   end
 
   def view_all_players_by_name
@@ -86,24 +76,7 @@ class CLIInterface
   end
 
   def create_team
-    puts "\n=== Create New Team ==="
-
-    print "Name: "
-    name = gets.chomp
-
-    print "City: "
-    city = gets.chomp
-
-    data = { name: name, city: city }
-
-    response = @api_client.create_team(data)
-
-    if response[:error]
-      puts "Error: #{response[:error]}"
-    else
-      puts "Congrats on creating your own NFL Team!!"
-      @display.display_team(response)
-    end
+    @team_ops.create
   end
 
   def create_player
@@ -138,35 +111,7 @@ class CLIInterface
   end
 
   def move_team_location
-    @display.show_teams_info
-    print "\nEnter the ID of the Team to move location: "
-    id = gets.chomp.to_i
-
-    team = @api_client.get_team(id)
-    if team[:error]
-      puts "Error: #{team[:error]}"
-      return
-    end
-
-    puts "\nCurrent Team data:"
-    @display.display_team(team)
-
-    puts "\nEnter new city (press Enter to keep current city):"
-
-    print "City (#{team['city']}): "
-    city = gets.chomp
-    city = team['city'] if city.empty?
-
-    data = { city: city }
-
-    response = @api_client.update_team(id, data)
-
-    if response[:error]
-      puts "Error: #{response[:error]}"
-    else
-      puts "Team location updated successfully!"
-      @display.display_team(response)
-    end
+    @team_ops.update_location
   end
 
   def trade_player
@@ -202,24 +147,7 @@ class CLIInterface
   end
 
   def delete_team
-    @display.show_teams_info
-    print "\nEnter the ID of the team to delete: "
-    id = gets.chomp.to_i
-
-    print "Are you sure you want to delete this team? It will delete all players associated with it! (y/n): "
-    confirmation = gets.chomp.downcase
-
-    if %w[y yes].include?(confirmation)
-      response = @api_client.delete_team(id)
-
-      if response[:error]
-        puts "Error: #{response[:error]}"
-      else
-        puts "Team deleted successfully!"
-      end
-    else
-      puts "Deletion cancelled."
-    end
+    @team_ops.delete
   end
 
   def delete_player
